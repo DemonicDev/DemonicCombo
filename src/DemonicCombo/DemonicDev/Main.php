216ee9a458fe8220d3ab63e9d3ec78525fa7227c
DemonicDev <61244099+DemonicDev@users.noreply.github.com>
2022-07-03 19:14:40 +0200

    Add files via upload

--- src/DemonicCombo/DemonicDev/Main.php
+++ src/DemonicCombo/DemonicDev/Main.php
@@ -4,15 +4,12 @@ namespace DemonicCombo\DemonicDev;
 use pocketmine\plugin\PluginBase; 
 use pocketmine\player\Player; 
 use pocketmine\Server; 
-use pocketmine\entity\Entity; 
 use pocketmine\event\entity\EntityDamageByEntityEvent; 
 use pocketmine\event\Listener; 
-use pocketmine\utils\TextFormat as tf; 
 
 class Main extends PluginBase implements Listener{
 	
 	public function onEnable() : void{ 
-		$this->getServer()->getLogger()->info(tf::WHITE. "[" .tf::DARK_RED. "Demonic" .tf::AQUA. "Combo" .tf::WHITE. "]" .tf::GREEN. " ENABLED"); 
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	} 
 	
